92aa114024ebfa14534f93ecb94be08c53e946b0
Matt Deasy <mattdeasy21@gmail.com>
2018-10-09 16:27:20 -0400

    Animals correspond to hour of the day instead of time worked

--- encourage.sh
+++ encourage.sh
@@ -1,16 +1,51 @@
 #!/bin/sh
+
 clear
 
-FILES=./images/*
+#Animal image corresponds to hours of the day
+#8-aardvark 9- 10- 11- 12- 1- 2- 3- 4- 5-Sad Face
 
+HOUR=$(date +%H)
+DIR=./images/
 IFS=''
-for filename in $FILES
-do
-  while read -r CHAR_IN; do
-     echo "$CHAR_IN"
-  done < $filename
-  export PS1=""
-  sleep 3600 #Time in seconds between switching images
-done
-echo "You've been at work for 8 hours!"
+
+time_to_animal(){
+
+	START=$HOUR
+
+	while read -r CHAR_IN; do
+     	echo "$CHAR_IN"
+  	done < $DIR/$1
+  	
+  	while [ $START -eq $HOUR ]; do
+  		sleep 60
+  	done
+}
+
+case $HOUR in
+
+	8) time_to_animal coffee
+	;;
+	9) time_to_animal bat
+	;;
+	10) time_to_animal bear
+	;;
+	11) time_to_animal beaver
+	;;
+	12) time_to_animal camel
+	;;
+	13) time_to_animal cow
+	;;
+	14) time_to_animal donkey
+	;;
+	15) time_to_animal horse
+	;;
+	16) time_to_animal aardvark
+	;;
+	17) time_to_animal aardvark
+	;;
+	*) time_to_animal sadface
+	;;
+esac
+
 export PS1="$"
